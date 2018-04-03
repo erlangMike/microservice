7d03c0e311aa3ed6e7746285cae6bce43256e461
jn <jiangnan.java@hotmail.com>
2018-04-03 17:31:48 +0800

    启动基础包

--- msc-consulserver/src/main/java/booter/SpringBootStartApplication.java
+++ msc-consulserver/src/main/java/booter/SpringBootStartApplication.java
@@ -3,11 +3,15 @@ package booter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author jn
  */
 @SpringBootApplication
+@EnableDiscoveryClient
+//@ComponentScan(basePackages = {"com.edu.msc.consul.*"})
 public class SpringBootStartApplication extends SpringBootServletInitializer {
 
     @Override
