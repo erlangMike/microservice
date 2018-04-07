41474e12b9fdaf006cf68ccd34403705feb25fe6
erlangMike <jiangnan.java@hotmail.com>
2018-04-07 22:55:42 +0800

    for ignore

--- msc-consulserver/src/main/java/booter/SpringBootStartApplication.java
+++ msc-consulserver/src/main/java/booter/SpringBootStartApplication.java
@@ -5,15 +5,23 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author jn
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
+
 //@ComponentScan(basePackages = {"com.edu.msc.consul.*"})
 public class SpringBootStartApplication extends SpringBootServletInitializer {
 
+    @RequestMapping("hi")
+    public String sayHello(){
+        return "Hi姜楠您好！";
+    }
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(SpringBootStartApplication.class);
